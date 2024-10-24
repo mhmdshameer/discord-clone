68f6868e7fcec95335281510146a7253ba201307
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-24 13:25:41 +0530

    feat(database): integrate Supabase with Prisma for database connection and configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme.provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 const geistSans = localFont({
@@ -27,11 +28,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={font.className}>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
-          enableSystem={false}
+          defaultTheme="system"
+          enableSystem
           storageKey="discord-theme">{children}</ThemeProvider>
         </body>
       </html>
