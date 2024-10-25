fd7e834b893cd99e8748e2abaf8eb8b307412559
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-25 20:32:15 +0530

    feat(InitialModal): create initial modal component for server customization

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,9 +1,18 @@
-'use client'
+"use client";
 
-export const InitialModal = () =>{
-    return (
-        <div>
-            This will be a modal!
-        </div>
-    )
-}
+import { DialogTitle } from "@radix-ui/react-dialog";
+import { Dialog, DialogContent, DialogHeader } from "../ui/dialog";
+
+export const InitialModal = () => {
+  return (
+    <Dialog open>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Customize your server
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
