b3810351f475c615aa61ae938ef2a32d4a578f25
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-26 13:51:38 +0530

    feat(Server): enable image upload functionality for server customization

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -23,6 +23,8 @@ import {
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
+import { FileUpload } from "../file-upload";
+
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -39,7 +41,7 @@ export const InitialModal = () => {
   useEffect(() => {
     setIsMounted(true);
   }, []);
-  
+
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -73,7 +75,20 @@ export const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Image Upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <FileUpload
+                        endpoint="serverImage"
+                        value={field.value}
+                        onChange={field.onChange}
+                      />
+                    </FormControl>
+                  </FormItem>)}
+                />
               </div>
               <FormField
                 control={form.control}
