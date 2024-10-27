4e13a731949fbf0ff77fd510e907b7e66a868959
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-10-27 11:42:46 +0530

    style(Sidebar): add styling to server channels layout

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,8 +1,26 @@
-export const NavigationSidebar = () => {
-    return (  
-        <div>
-            NavigationSidebar
-        </div>
-    );
-}
- 
+import { currentProfile } from "@/lib/current-profile";
+import prisma from "@/lib/db";
+import { redirect } from "next/dist/server/api-utils";
+
+export const NavigationSidebar = async () => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const servers = await prisma.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  return (
+    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[1E1F22] py-3">
+      NavigationSidebar
+    </div>
+  );
+};
