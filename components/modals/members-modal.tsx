cca899ea013ab9b5ff6e5ec23b883cdf9e884ff9
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-14 11:50:02 +0530

    feat(MembersModal): add role selection with guest option in dropdown menu

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -6,9 +6,9 @@ import { DialogTitle, Dialog, DialogContent, DialogHeader, DialogDescription } f
 import { useModal } from "@/hooks/use-modal-store";
 import { ScrollArea } from "../ui/scroll-area";
 import { UserAvatar } from "../user-avatar";
-import { MoreVertical, ShieldAlert, ShieldCheck } from "lucide-react";
+import { Check, MoreVertical, Shield, ShieldAlert, ShieldCheck, ShieldQuestion } from "lucide-react";
 import { useState } from "react";
-import { DropdownMenu, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuPortal, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from "../ui/dropdown-menu";
 
 const roleIconMap = {
   "GUEST": null,
@@ -53,6 +53,25 @@ export const MembersModal = () => {
                   <DropdownMenuTrigger>
                     <MoreVertical className="h-4 w-4 text-zinc-500"/>
                   </DropdownMenuTrigger>
+                  <DropdownMenuContent side="left">
+                    <DropdownMenuSub>
+                      <DropdownMenuSubTrigger className="flex items-center">
+                          <ShieldQuestion className="w-4 h-4 mr-2"/>
+                          <span>Role</span>
+                      </DropdownMenuSubTrigger>
+                      <DropdownMenuPortal>
+                        <DropdownMenuSubContent>
+                          <DropdownMenuItem>
+                            <Shield className="h-4 w-4 mr-2"/>
+                            Guest
+                            {member.role === "GUEST" && (
+                              <Check className="h-4 w-4 ml-auto"/>
+                            )}
+                          </DropdownMenuItem>
+                        </DropdownMenuSubContent>
+                      </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                  </DropdownMenuContent>
                 </DropdownMenu>
               </div>
             )}
