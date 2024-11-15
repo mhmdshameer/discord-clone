b9c890d056b0124dabc0c2b51f4196a659e99e01
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-15 09:08:03 +0530

    feat(CreateChannelModal): add new modal for creating channels with form validation

--- app/api/members/[memberId]/route.ts
+++ app/api/members/[memberId]/route.ts
@@ -2,6 +2,61 @@ import { currentProfile } from "@/lib/current-profile";
 import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { memberId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!serverId) {
+      return new NextResponse("Server id is missing", { status: 400 });
+    }
+    if (!params.memberId) {
+      return new NextResponse("Member id is missing", { status: 400 });
+    }
+
+    const server = await prisma.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        members: {
+          deleteMany: {
+            id: params.memberId,
+            profileId: {
+              not: profile.id,
+            },
+          },
+        },
+      },
+      include:{
+        members:{
+            include:{
+                profile: true,
+            },
+            orderBy:{
+                role: "asc"
+            }
+        }
+      }
+    });
+    return NextResponse.json(server);
+    
+  } catch (error) {
+    console.log("[MEMBER_ID_DELETE]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { memberId: string } }
@@ -56,7 +111,7 @@ export async function PATCH(
       },
     });
 
-    return NextResponse.json(server)
+    return NextResponse.json(server);
   } catch (error) {
     console.log("MEMBERS_ID_PATCH", error);
     return new NextResponse("Internal Error", { status: 500 });
