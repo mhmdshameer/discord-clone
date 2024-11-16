e76f6ac7a6b17c6c4b2781a3cef64359482f641f
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-16 12:10:01 +0530

    feat(ServerSearch): add Ctrl+K shortcut and navigation based on type and ID

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { Search } from "lucide-react";
-import { useState } from "react";
+import { KeyboardEvent, useEffect, useState } from "react";
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "../ui/command";
 import { DialogTitle } from "../ui/dialog";
+import { useParams, useRouter } from "next/navigation";
 
 interface ServerSearchProps {
   data: {
@@ -21,6 +22,32 @@ interface ServerSearchProps {
 
 export const ServerSearch = ({ data }: ServerSearchProps) => { 
     const [open, setOpen ] = useState(false)
+    const router = useRouter()
+    const params = useParams()
+
+    useEffect(()=>{
+        const down = (e: KeyboardEvent)=>{
+            if (e.key=== "k" && (e.metaKey || e.ctrlKey))
+            {e.preventDefault();
+                setOpen((open)=> !open)
+            }
+        }
+
+        document.addEventListener("keydown",down);
+        return ()=> document.removeEventListener("keydown", down)
+    },[]);
+
+    const onClick = ({id, type}: {id: string, type: "channel" | "member"}) =>{
+        setOpen(false)
+
+        if(type === "member"){
+            return router.push(`/servers/${params?.serverId}/conversations/${id}`)
+        }
+
+        if(type === "channel"){
+            return router.push(`/servers/${params?.serverId}/channels/${id}`)
+        }
+    }
   return (
     <>
       <button
@@ -37,7 +64,7 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
         </kbd>
       </button>
       <CommandDialog open={open} onOpenChange={setOpen} >
-          <DialogTitle className="m-4">Search Dialog</DialogTitle>
+          <DialogTitle className="m-4 hidden">Search</DialogTitle>
         <CommandInput placeholder="Search all channels and members" />
         <CommandList>
             <CommandEmpty>
@@ -50,7 +77,7 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
                     <CommandGroup key={label} heading={label}>
                         {data.map(({id, icon, name})=>{
                             return (
-                               <CommandItem key={id}>
+                               <CommandItem key={id} onSelect={()=> onClick({id,type})}>
                                 {icon}
                                 <span>{name}</span>
                                </CommandItem>
