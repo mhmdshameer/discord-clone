9d6dc2c04d354081d3488d9d86239adcf49e6c13
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-17 13:01:50 +0530

    feat(deleteChannel): Admin and moderator can delete channel

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -5,6 +5,7 @@ import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
 import {  Hash, Lock, Mic, Settings, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { ActionTooltip } from "../action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -23,6 +24,7 @@ export const ServerChannel = ({
   server,
   role,
 }: ServerChannelProps) => {
+    const {onOpen} = useModal()
   const router = useRouter();
   const params = useParams();
 
@@ -52,7 +54,7 @@ export const ServerChannel = ({
             <Settings className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
           </ActionTooltip>
           <ActionTooltip label="Delete">
-            <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+            <Trash onClick={()=> onOpen("deleteChannel",{server, channel})} className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
           </ActionTooltip>
         </div>
       )}
