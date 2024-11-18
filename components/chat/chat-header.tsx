3b7d4e5ad7fd049819e68b60953e56b0a7828995
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-18 08:07:54 +0530

    feat(MobileToggle): create a new component for mobile

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,4 +1,5 @@
 import { Hash, Menu } from "lucide-react";
+import { MobileToggle } from "../mobile-toggle";
 
 interface ChatHeaderProps{
     serverId: string;
@@ -14,10 +15,13 @@ export const ChatHeader = ({
 }:ChatHeaderProps) =>{
     return (
         <div className="text-base font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-        <Menu/>
+        <MobileToggle/>
         {type === "channel" && (
             <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2"/>
         )}
+            <p className="font-semibold text-base text-black dark:text-white">
+                {name}
+            </p>
         </div>
     )
 }
