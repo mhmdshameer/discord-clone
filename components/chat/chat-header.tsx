95671fd9a914a5b6c7a12e0226de032b42e1e6e7
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-18 08:27:33 +0530

    feat(MobileToggle):make toggle button functional

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,4 +1,4 @@
-import { Hash, Menu } from "lucide-react";
+import { Hash } from "lucide-react";
 import { MobileToggle } from "../mobile-toggle";
 
 interface ChatHeaderProps{
@@ -15,7 +15,7 @@ export const ChatHeader = ({
 }:ChatHeaderProps) =>{
     return (
         <div className="text-base font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-        <MobileToggle/>
+        <MobileToggle serverId={serverId}/>
         {type === "channel" && (
             <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2"/>
         )}
--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -1,7 +1,23 @@
-import { Menu } from "lucide-react"
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Button } from "./ui/button";
+import { ServerSidebar } from "./server/server-sidebar";
+import { NavigationSidebar } from "./navigation/navigation-sidebar";
 
-export const MobileToggle=()=>{
-    return (
-       <Menu/>
-    )
-}
+export const MobileToggle = ({serverId}:{serverId: string}) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant='ghost' size="icon" className="">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <div>
+        <NavigationSidebar/>
+        </div>
+        <ServerSidebar serverId={serverId}/>
+      </SheetContent>
+    </Sheet>
+  );
+};
