f280a949af5abe216f64c2c59c972341d3160503
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-20 15:32:03 +0530

    feat(SocketIndicators): create component for socket indicators

---
+++ components/socket-indicators.tsx
@@ -0,0 +1,19 @@
+import { useSocket } from "./providers/socket-providers";
+import { Badge } from "./ui/badge";
+
+export const SocketIndicators = () => {
+  const { isConnected } = useSocket();
+
+  if (!isConnected) {
+    return (
+      <Badge variant="outline" className="bg-yellow-600 text-white border-none">
+        Fallback: Polling every 1s
+      </Badge>
+    );
+  }
+  return (
+    <Badge variant="outline" className="bg-emerald-600 text-white border-none">
+      Live: Real time updates
+    </Badge>
+  );
+};
