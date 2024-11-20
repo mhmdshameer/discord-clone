fcf8f60754bf3705f005e32302f0a050110ac8fc
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-20 16:46:01 +0530

    feat(ChannelIdPage): pass all data need to ChatInput

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,6 +1,24 @@
 "use client";
 
-export const ChatInput = () =>{
+import * as z from "zod";
+
+interface ChatInputProps {
+    apiUrl: string;
+    query: Record <string, any>;
+    name: string;
+    type: "conversation" | "channel";
+}
+
+const formSchema= z.object({
+    content: z.string().min(1)
+})
+
+export const ChatInput = ({
+    apiUrl,
+    query,
+    name, 
+    type
+}:ChatInputProps) =>{
     return(
         <div>
             Chat Header!
