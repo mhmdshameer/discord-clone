079d5c3fed25170cc118eb8b94d2e432938f9f31
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-22 18:43:20 +0530

    feat(file-uplaod): made some changes for upload pdf

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { UploadDropzone } from "@/lib/uploadthing";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import "@uploadthing/react/styles.css";
@@ -13,8 +13,10 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
-  console.log("value:", value)
+
+  console.log("field:", value);
   const fileType = value?.split(".").pop();
+  console.log("fileType:",value)
   if (value && fileType !== "pdf") {
     return (
       <div className="relative h-20 w-20">
@@ -28,6 +30,23 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
         </button>
       </div>
     );
+  }
+  if(value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400"/>
+        <a href={value} target="_blank" rel="noopener noreferrer" className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline">
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    )
   }
   return (
     <div>
