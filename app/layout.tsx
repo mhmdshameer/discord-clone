ff55475338b92f498d21d16679c12698b98263fd
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-23 11:00:21 +0530

    feat(useChatQuery): create a hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider } from "@/components/providers/theme.provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-providers";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -28,7 +29,7 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>{children}</QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -2,6 +2,8 @@
 
 import { Member } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { Loader2 } from "lucide-react";
 
 interface ChatMessagesProps{
     name: string;
@@ -16,6 +18,19 @@ interface ChatMessagesProps{
 }
 
 export const ChatMessages = ({name, member, chatId, apiUrl, socketUrl, socketQuery, paramKey, paramValue, type}: ChatMessagesProps) => {
+    const queryKey = `chat:${chatId}`;
+    const {data, fetchNextPage, hasNextPage, isFetchingNextPage, status} = useChatQuery({
+        queryKey, apiUrl, paramKey, paramValue
+    })
+
+    if(status === "loading"){
+        return(
+            <div className="flex flex-col flex-1 justify-center items-center">
+<Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4"/>
+            </div>
+        )
+    }
+    
     return(
         <div className="flex-1 flex  flex-col py-4 overflow-y-auto">
             <div className="flex-1"/>
