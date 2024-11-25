d277c692d452b92a2e98281822be19d922130e51
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-25 10:45:44 +0530

    feat(GET meesages): create get message route in messages

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -5,7 +5,7 @@ import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 
-interface ChatMessagesProps{
+interface ChatMessagesProps {
     name: string;
     member: Member;
     chatId: string;
@@ -17,37 +17,44 @@ interface ChatMessagesProps{
     type: "channel" | "conversation";
 }
 
-export const ChatMessages = ({name, member, chatId, apiUrl, socketUrl, socketQuery, paramKey, paramValue, type}: ChatMessagesProps) => {
+export const ChatMessages = ({
+    name,
+    member,
+    chatId,
+    apiUrl,
+    socketUrl,
+    socketQuery,
+    paramKey,
+    paramValue,
+    type,
+}: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`;
-    const {data, fetchNextPage, hasNextPage, isFetchingNextPage, status} = useChatQuery({
+    const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading, isError } = useChatQuery({
         queryKey, apiUrl, paramKey, paramValue
-    })
+    });
 
-    if(status === "loading"){
-        return(
+    if (isLoading) {
+        return (
             <div className="flex flex-col flex-1 justify-center items-center">
-<Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4"/>
-<p className="text-xs text-zinc-500 dark:text-zinc-400">Loading messages...</p>
+                <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">Loading messages...</p>
             </div>
-        )
+        );
     }
-    if(status === "error"){
-        return(
+
+    if (isError) {
+        return (
             <div className="flex flex-col flex-1 justify-center items-center">
-<ServerCrash className="h-7 w-7 text-zinc-500  my-4"/>
-<p className="text-xs text-zinc-500 dark:text-zinc-400">Something went wrong!</p>
+                <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">Something went wrong!</p>
             </div>
-        )
+        );
     }
-    
-    return(
-        <div className="flex-1 flex  flex-col py-4 overflow-y-auto">
-            <div className="flex-1"/>
-            <ChatWelcome
-            type={type}
-            name={name}
-            />
-           
+
+    return (
+        <div className="flex-1 flex flex-col py-4 overflow-y-auto">
+            <div className="flex-1" />
+            <ChatWelcome type={type} name={name} />
         </div>
-    )
-}
+    );
+};
