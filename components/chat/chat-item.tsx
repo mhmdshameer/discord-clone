0fb1fb6abf22425c93ab14a7050688574d8cdf11
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-26 09:01:12 +0530

    feat(ChatItem): show member role next to member name

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -2,6 +2,8 @@
 
 import { Member, Profile } from "@prisma/client";
 import { UserAvatar } from "../user-avatar";
+import { ActionTooltip } from "../action-tooltip";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
 
 interface ChatItemProps{
     id: string;
@@ -18,6 +20,12 @@ interface ChatItemProps{
     socketQuery: Record <string, string>;
 }
 
+const roleIconMap = {
+    "GUEST": null,
+    "MODERATOR": <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500"/>,
+    "ADMIN": <ShieldAlert className="h-4 w-4 ml-2 text-rose-500"/>
+}
+
 export const ChatItem = ({id,content,member,timestamp,fileUrl,deleted,currentMember,isUpdated,socketUrl,socketQuery}:ChatItemProps) => {
     return (
         <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
@@ -31,6 +39,9 @@ export const ChatItem = ({id,content,member,timestamp,fileUrl,deleted,currentMem
                             <p className="font-semibold text-sm hover:underline cursor-pointer">
                                 {member.profile.name}
                             </p>
+                            <ActionTooltip label={member.role}>
+                                {roleIconMap[member.role]}
+                            </ActionTooltip>
                         </div>
                     </div>
                 </div>
