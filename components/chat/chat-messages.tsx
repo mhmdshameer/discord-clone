d3076b618972b0d29cb3c3a299870a9bf16573f5
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-11-26 08:37:54 +0530

    feat(ChatItem): add ChatItem component structure with props integration

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { Member, Message, Profile } from "@prisma/client";
+import {format} from "date-fns";
 import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import { ChatItem } from "./chat-item";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 type MessageWithMemberWithProfile = Message & {
   member: Member & {
@@ -81,7 +84,19 @@ export const ChatMessages = ({
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <ChatItem />
+              <ChatItem
+              key={message.id}
+              id={message.id}
+              currentMember={member}
+              member={message.member}
+              content={message.content}
+              fileUrl={message.fileUrl}
+              deleted={message.delete}
+              timestamp={format(new Date(message.createdAt),DATE_FORMAT)}
+              isUpdated={message.updatedAt !== message.createdAt}
+              socketUrl={socketUrl}
+              socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
